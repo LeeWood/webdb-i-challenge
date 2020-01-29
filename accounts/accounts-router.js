2980f337c285cee1154e868e7e529ca304f2d1c7
lee-wood <leemaewood@gmail.com>
2020-01-29 17:32:56 -0500

    added post and put queries

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -6,12 +6,12 @@ const router = express.Router();
 
 //GET
 
-router.get('/', async (req,res) => { //GET all accts
-//SELECT * FROM Accounts;
+router.get('/', async (req,res) => { //* GET all accts (SELECT * FROM Accounts;)
+
   try{
-    const [accounts] = await db('accounts');
+    const accounts = await db('accounts');
     res.status(200).json(accounts);
-  } catch(err) {
+  }catch(err) {
     res.status(500).json({
       message: "There's been a problem retrieving accounts.",
       err
@@ -19,13 +19,14 @@ router.get('/', async (req,res) => { //GET all accts
   }
 });
 
-router.get('/:id', async (req, res) => { //GET specific acct
-  //SELECT * FROM Accountrs WHERE id = i;
+router.get('/:id', async (req, res) => { //* GET specific acct (SELECT * FROM Accountrs WHERE id = i;)
+  
   const { id } = req.params;
+
   try {
-    const [account] = await db('accounts').where('id', id);
+    const account = await db('accounts').where('id', id);
     res.status(200).json(account);
-  } catch(err) {
+  }catch(err) {
     res.status(500).json({
       message: "There's been a problem retrieving account info.",
       err
@@ -36,9 +37,41 @@ router.get('/:id', async (req, res) => { //GET specific acct
 
 //POST
 
+router.post('/', async (req, res) => { //* POST new account (INSERT INTO Accounts (name, budget) VALUES (inputName, inputValue);)
+  
+  const acctData = req.body;
 
+  try {
+    const account = await db('accounts').insert(acctData);
+    //TODO: see if you can add if statements for what returns here. Conditionals...name and budget required...maybe middleware later during class. Name must be unique. Budget must be a number
+    res.status(201).json(account);
+  }catch(err) {
+    res.status(500).json({
+      message: "There's been a problem adding new account.",
+      err
+    });
+  }
+})
 
 //PUT
+
+router.put('/:id', async (req, res) => { //*PUT existing account (UPDATE Accounts SET name/budget = Value WHERE id = i;)
+
+  const { id } = req.params;
+  const updatedInfo = req.body;
+
+  try {
+    const rowsUpdated = await db('accounts').where('id', id).update(updatedInfo);
+    res.status(204).json({
+      updated: rowsUpdated
+    }); 
+  }catch(err) {
+    res.status(500).json({
+      message: "There has been a problem editing account.",
+      err
+    });
+  }
+});
 
 //DELETE
 
